2ac74b49eecc9fb1f8067442fcca0d23c10cce88
Prajwal Bati <prajwalbati@gmail.com>
2023-10-03 13:42:01 -0400

    refactor and optimize

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+
+const path = require("path");
 const express = require('express');
-const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 
-app.use(cors());
+app.use(express.static('public'));
 
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/download', (req, res) => {
     var URL = req.query.URL;
-    
+
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 
     ytdl(URL, {
         format: 'mp4'
     }).pipe(res);
+});
+
+app.listen(4000, () => {
+    console.log('Server listening in the port 4000');
 });
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,5 +5,5 @@ convertBtn.addEventListener('click', () => {
     sendURL(URLinput.value);
 });
 function sendURL(URL) {
-    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+    window.location.href = `/download?URL=${URL}`;
 }
